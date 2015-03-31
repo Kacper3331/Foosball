9f4d07e72dd93d8fb6de8a375207a7227d301c0b
kacper331 <denebv201@gmail.com>
2015-03-31 12:16:22 +0200

    Add validations to match model fields.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,18 @@
 class Match < ActiveRecord::Base
   has_many :players
 
+  validates :first_player_score,
+    numericality: true,
+    presence: true
+
+  validates :second_player_score,
+    numericality: true,
+    presence: true
+
+  validates :date,
+    presence: true
+
+  validates :players,
+    presence: true
+
 end
