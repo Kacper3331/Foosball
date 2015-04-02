004b16ed0f219763dd5d9b317f56ae5ddae1f3b6
kacper331 <denebv201@gmail.com>
2015-04-02 21:35:14 +0200

    Create method with recent played match to player page view.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,9 +2,30 @@ class PlayersController < ApplicationController
   before_action :set_player, only: [:show]
 
   def show
+      @recent_match = recent_match(player)
   end
 
   private
+  def recent_match(player)
+    recent_match_first = Match.select(:first_player_id, 'first_player_score as score' ,'second_player_id as oponent', :date).where(first_player_id: player.id)
+    recent_match_second = Match.select(:second_player_id, 'second_player_score as score' ,'first_player_id as oponent', :date).where(second_player_id: player.id)
+
+    if recent_match_first.empty?
+      return @match = recent_match_second.last
+    end
+
+    if recent_match_second.empty?
+      return @match = recent_match_first.last
+    end
+
+    if !recent_match_first.empty? && !recent_match_second.empty?
+      if recent_match_first.last.date < recent_match_second.last.date
+        @match = recent_match_second.last
+      else
+        @match = recent_match_first.last
+      end
+    end
+  end
   def set_player
     @player = Player.find(params[:id])
   end
