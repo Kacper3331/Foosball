1b71f867f06bf63174c4a0b1c98a444bdb1158f5
kacper331 <denebv201@gmail.com>
2015-04-02 21:42:31 +0200

    Create specs for match model.

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to respond_to :date }
+  it { is_expected.to respond_to :first_player_score }
+  it { is_expected.to respond_to :second_player_score }
+  it { is_expected.to respond_to :players }
+
+  it { is_expected.to validate_presence_of(:date) }
+  it { is_expected.to validate_presence_of(:first_player_score) }
+  it { is_expected.to validate_presence_of(:second_player_score) }
+  it { is_expected.to validate_presence_of(:players) }
 end
