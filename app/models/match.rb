54c381abab0c8cf2d78ba53e127199e3d8833f5e
kacper331 <denebv201@gmail.com>
2015-04-03 20:39:01 +0200

    Create scope with information when player is played as first player.

--- app/models/match.rb
+++ app/models/match.rb
@@ -16,4 +16,8 @@ class Match < ActiveRecord::Base
     match.first_player_id = player_ids.first.to_i
     match.second_player_id = player_ids.last.to_i
   end
+
+  scope :select_first_player, -> {
+    select(:first_player_id, 'first_player_score as score' ,'second_player_id as oponent', :date)
+  }
 end
