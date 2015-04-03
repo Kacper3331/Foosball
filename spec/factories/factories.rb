a87c7ebb5b2b295e532e8dfbd53c1bdb99da4e72
kacper331 <denebv201@gmail.com>
2015-04-03 20:51:30 +0200

    Update factories.

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -7,11 +7,18 @@ FactoryGirl.define do
     lastname 'Nowak'
   end
 
+  factory :second_player, class: Player do
+    id 2
+    email 'second_example@example.pl'
+    password 'example123'
+    firstname 'Jan'
+    lastname 'Kowalski'
+  end
+
   factory :match do
-    first_player_id 1
-    second_player_id 2
     first_player_score 10
     second_player_score 8
     date "2015-03-25"
+    association :players, factory: :player
   end
 end
